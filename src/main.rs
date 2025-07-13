7bdc3b0460f4653109294b6689072be4431bad6e
Gabriel Barros <gabreilbarros13@gmail.com>
2025-07-12 23:49:19 -0300

    Feat(Main): added new route nodes

--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,16 @@ mod services;
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     infra::environment::settings_logger();
-    infra::database::migrations_up();
+    infra::database::DatabaseHandler::migrations_up();
 
-    let pool_handler= infra::database::PoolHandler::new();
+    let pool_handler= infra::database::DatabaseHandler::new();
 
     HttpServer::new(move || {
         App::new()
             .app_data(web::Data::new(pool_handler.clone()))
             .route("/", web::get().to(services::status::execute))
             .route("/status", web::get().to(services::status::execute))
+            .route("/nodes", web::get().to(services::node::execute))
     })
     .bind(("0.0.0.0", infra::environment::get_environments().port))?
     .run()
