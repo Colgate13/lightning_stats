19d5e90521be5f21f7543c4ec4b9a33970b5b786
Gabriel Barros <gabreilbarros13@gmail.com>
2025-07-13 18:16:44 -0300

    Feat(Node): added new models in node

--- src/models/node.rs
+++ src/models/node.rs
@@ -5,11 +5,45 @@ use chrono::{DateTime, Utc};
 
 use crate::infra::schema::nodes;
 
-#[derive(Queryable, Serialize, Deserialize, Selectable, Debug)]
+#[derive(Queryable, Insertable, Serialize, Deserialize, Selectable, Debug)]
 #[diesel(table_name = nodes)]
 pub struct Node {
   pub public_key: String,
   pub alias: String,
   pub capacity: BigDecimal,
   pub first_seen: DateTime<Utc>,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct ResponseNodes {
+  #[serde(rename = "publicKey")]
+  public_key: String,
+  alias: String,
+  capacity: i64,
+  #[serde(rename = "firstSeen")]
+  first_seen: i64
+}
+
+impl Node {
+  fn satoshis_to_bitcoin(satoshis: i64) -> BigDecimal {
+    let sats = BigDecimal::from(satoshis);
+    let bitcoin_factor = BigDecimal::from(100_000_000);
+    sats / bitcoin_factor
+  }
+
+  fn timestamp_to_datetime(timestamp: i64) -> DateTime<Utc> {
+    DateTime::from_timestamp(timestamp, 0)
+      .unwrap_or_else(Utc::now)
+  }
+}
+
+impl From<ResponseNodes> for Node {
+  fn from(value: ResponseNodes) -> Self {
+    Node {
+      public_key: value.public_key,
+      alias: value.alias,
+      capacity: Node::satoshis_to_bitcoin(value.capacity),
+      first_seen: Node::timestamp_to_datetime(value.first_seen)
+    }
+  }
 }
